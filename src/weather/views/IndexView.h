ba11f05987a384f6c203be3e397037383a841f79
亨睿 <henry.lx@alibaba-inc.com>
2018-08-29 14:51:39 +0800

    Add subviews

--- src/weather/views/IndexView.h
+++ src/weather/views/IndexView.h
@@ -6,13 +6,26 @@
 
 #include "../../assets/icon-fonts.h"
 
+#include "./WeatherForecastView.h"
+
 class WeatherIndexView : public View {
 public:
   void render(CanvasContext *context) {
     _drawContent(context);
   }
 
+  void didSelect() {
+    Application.pushView(getForecastView());
+  }
+
 private:
+  WeatherForecastView *getForecastView() {
+    if (_forecastView == NULL) {
+      _forecastView = new WeatherForecastView();
+    }
+    return _forecastView;
+  }
+
   void _drawContent(CanvasContext *context) {
     context->setFontSize(FontSize::NORMAL);
     context->setTextAlign(TextAlign::LEFT);
@@ -27,4 +40,6 @@ private:
     int weatherIconWidth = context->getStringWidth(weatherIcon);
     context->drawString(weatherIcon, 32 - weatherIconWidth / 2, 9);
   }
+
+  WeatherForecastView *_forecastView = NULL;
 };
